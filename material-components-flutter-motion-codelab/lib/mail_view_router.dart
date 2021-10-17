b45e4c2cc63c09288e7ca2f3f10cae08d2088ae7
SnoopyKim <snoopykim0@gmail.com>
2021-10-17 17:39:44 +0900

    Building Beautiful Transitions with Material Motion for Flutter: done

--- material-components-flutter-motion-codelab/lib/mail_view_router.dart
+++ material-components-flutter-motion-codelab/lib/mail_view_router.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -27,11 +28,9 @@ class MailViewRouterDelegate extends RouterDelegate<void>
           onPopPage: _handlePopPage,
           pages: [
             // TODO: Add Fade through transition between mailbox pages (Motion)
-            CustomTransitionPage(
+            FadeThroughTransitionPageWrapper(
+              mailbox: InboxPage(destination: currentlySelectedInbox),
               transitionKey: ValueKey(currentlySelectedInbox),
-              screen: InboxPage(
-                destination: currentlySelectedInbox,
-              ),
             )
           ],
         );
@@ -44,8 +43,7 @@ class MailViewRouterDelegate extends RouterDelegate<void>
 
   @override
   Future<bool> popRoute() {
-    var emailStore =
-        Provider.of<EmailStore>(navigatorKey.currentContext!, listen: false);
+    var emailStore = Provider.of<EmailStore>(navigatorKey.currentContext!, listen: false);
     bool onCompose = emailStore.onCompose;
 
     bool onMailView = emailStore.onMailView;
@@ -71,8 +69,7 @@ class MailViewRouterDelegate extends RouterDelegate<void>
     // Handles the back button when on the [ComposePage].
     if (onCompose) {
       // TODO: Add Container Transform from FAB to compose email page (Motion)
-      emailStore.onCompose = false;
-      return SynchronousFuture<bool>(false);
+      return SynchronousFuture<bool>(true);
     }
 
     // Handles the back button when the bottom drawer is visible on the
@@ -103,3 +100,29 @@ class MailViewRouterDelegate extends RouterDelegate<void>
 }
 
 // TODO: Add Fade through transition between mailbox pages (Motion)
+class FadeThroughTransitionPageWrapper extends Page {
+  const FadeThroughTransitionPageWrapper({
+    required this.mailbox,
+    required this.transitionKey,
+  }) : super(key: transitionKey);
+
+  final Widget mailbox;
+  final ValueKey transitionKey;
+
+  @override
+  Route createRoute(BuildContext context) {
+    return PageRouteBuilder(
+        settings: this,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          return FadeThroughTransition(
+            fillColor: Theme.of(context).scaffoldBackgroundColor,
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child,
+          );
+        },
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return mailbox;
+        });
+  }
+}
